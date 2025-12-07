fff37bcf371591a93c4a59b3d91c60e795552e08
baslani <behzadaslani80@gmail.com>
2025-12-07 16:27:20 +0330

    added env

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,6 @@ import { UsersModule } from './users/users.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      // database: process.env.NODE_ENV === 'development' ? 'db.sqlite' : 'test.sqlite',
       database: 'db.sqlite',
       entities: [User, Report],
       synchronize: true, // use only in development
@@ -32,7 +31,7 @@ export class AppModule {
     consumer
       .apply(
         cookieSession({
-          keys: ['qwerty'],
+          keys: [process.env.COOKIE_SESSION_KEY!],
         }),
       )
       .forRoutes('*');
